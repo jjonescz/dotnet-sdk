fedf7354bfd0c203c6988ae880ada94ac33b7b33
Jan Jones <janjones@microsoft.com>
2024-08-19 16:08:45 +0200

    Turn it into VSIX project

--- src/Microsoft.Net.Sdk.AnalyzerRedirecting/RedirectingAnalyzerAssemblyResolver.cs
+++ src/Microsoft.Net.Sdk.AnalyzerRedirecting/RedirectingAnalyzerAssemblyResolver.cs
@@ -1,13 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable enable
-
 using Microsoft.CodeAnalysis;
+using System.ComponentModel.Composition;
 using System.Reflection;
 
 namespace Microsoft.Net.Sdk.AnalyzerRedirecting;
 
+[Export(typeof(IAnalyzerAssemblyResolver))]
 public sealed class RedirectingAnalyzerAssemblyResolver : IAnalyzerAssemblyResolver
 {
     public Assembly? ResolveAssembly(AssemblyName assemblyName, string assemblyOriginalDirectory)
