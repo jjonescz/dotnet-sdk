78a3c23482b5724f23fee218346bef791627e6c2
Jan Jones <janjones@microsoft.com>
2024-08-20 12:27:07 +0200

    Add a test project

---
+++ test/Microsoft.Net.Sdk.AnalyzerRedirecting.Tests/RedirectingAnalyzerAssemblyResolverTests.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+
+namespace Microsoft.Net.Sdk.AnalyzerRedirecting.Tests;
+
+public class RedirectingAnalyzerAssemblyResolverTests
+{
+    [Fact]
+    public void Redirects()
+    {
+        Debug.Fail(TestContext.Current.TestAssetsDirectory);
+    }
+}
