7a4286db203fdcdae0617c74f99feb2b741b5785
Jan Jones <janjones@microsoft.com>
2025-04-28 15:11:44 +0200

    Remove unused API option

--- src/Cli/dotnet/Commands/Project/Convert/ProjectConvertCommandParser.cs
+++ src/Cli/dotnet/Commands/Project/Convert/ProjectConvertCommandParser.cs
@@ -22,15 +22,6 @@ internal sealed class ProjectConvertCommandParser
         Arity = ArgumentArity.Zero,
     };
 
-    /// <summary>
-    /// API mode. Takes input as JSON, produces JSON, doesn't perform any changes. Used by IDE to see the project file behind a file-based program.
-    /// </summary>
-    public static readonly Option<bool> ApiOption = new("--api")
-    {
-        Hidden = true,
-        Arity = ArgumentArity.Zero,
-    };
-
     public static Command GetCommand()
     {
         Command command = new("convert", CliCommandStrings.ProjectConvertAppFullName)
@@ -38,7 +29,6 @@ internal sealed class ProjectConvertCommandParser
             FileArgument,
             SharedOptions.OutputOption,
             ForceOption,
-            ApiOption,
         };
 
         command.SetAction((parseResult) => new ProjectConvertCommand(parseResult).Execute());
